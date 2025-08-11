cbc967dab97bd43ecfc280cbbefcd76cd3dfd9a2
Kirill Tepljakov <super.css2017@yandex.ru>
2025-08-11 23:18:47 +0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <div class="header">
+    <div class="wrapper">
+      <div class="title"><h1>Это заголовок</h1></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  width: 100%;
+  height: 91px;
+  background-color: var(--blue-color);
+}
+</style>
