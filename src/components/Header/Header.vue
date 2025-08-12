b3e2f166c3f11d3222114d5fbacc71bc98299f0b
KIRILL TEPLYAKOV <49129331+TepljakovKirill@users.noreply.github.com>
2025-08-12 09:38:52 +0300

    added and configured scss

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,12 @@
+<script setup>
+import style from './header.module.scss'
+</script>
+
+<template>
+  <div :class="style.header">
+    <div class="wrapper">
+      <div :class="title"><h1>Это заголовок</h1></div>
+    </div>
+  </div>
+</template>
+
