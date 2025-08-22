b988e125440883332ace2f4f4456ae0a55d3b7c1
KIRILL TEPLYAKOV <49129331+TepljakovKirill@users.noreply.github.com>
2025-08-22 16:04:41 +0300

    start development ProductCard

---
+++ src/components/ProductCard/ProductCard.vue
@@ -0,0 +1,21 @@
+<script setup>
+import style from './productCard.module.scss'
+import image from '@/assets/images/card-1.jpg'
+</script>
+
+<template>
+  <div :class="style.productCard">
+    <img :src="image" alt="Картинка">
+    <div :class="style.productCard_description">
+      <span :class="style.productCard_title">Костюм для девочки</span>
+      <div :class="style.price_block">
+        <span :class='style.price'>599 ₽</span>
+        <span :class='style.price_old'>719 ₽</span>
+      </div>
+      <div :class="style.productCard_block_button">
+        <button :class="style.view_button">Смотреть</button>
+        <button :class="style.cart_button">В корзину</button>
+      </div>
+    </div>
+  </div>
+</template>
--- src/components/Stock/Stock.vue
+++ src/components/Stock/Stock.vue
@@ -1,5 +1,6 @@
 <script setup>
 import style from './stock.module.scss'
+import ProductCard from '../ProductCard/ProductCard.vue';
 </script>
 
 <template>
@@ -8,6 +9,7 @@ import style from './stock.module.scss'
         <div class="border">
           <div :class="style.stock_container">
             <h2 class='title-section'>Акции</h2>
+            <ProductCard />
           </div>
         </div>
       </div>
